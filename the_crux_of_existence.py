d21ed1f95808a0b28ce961585421872763e81c16
yasmeennahas <106609278+yasmeennahas@users.noreply.github.com>
2023-04-19 20:28:58 -0500

    Update the_crux_of_existence.py

--- the_crux_of_existence.py
+++ the_crux_of_existence.py
@@ -6,55 +6,76 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats._stats_py
 
+## DISCONTINUITY STATS
 
-## PERM ENTROPY DATA ANALYSIS
 
 import_path = r'C:\Users\Yasmeen\Desktop\thesis_project\results\data_real_disc.csv'
 
 df = pd.read_csv(import_path)
 
-df = df.loc[df.task != 'EO']
+df = df.loc[df.task == 'EC']
 
-# Averaging across channel complexity
 
-no_channels = df.groupby(["subject", "session", "task"])["complexity"].mean().reset_index()
+# Correlation
+
+results = (df.groupby)(["subject"])[["complexity", "discontinuity"]].mean().reset_index()
+results = pg.rcorr(results)
+
+# Export correlation results
+results.to_csv('C:/Users/Yasmeen/Desktop/thesis_project/results/final_all/correlation.csv')
+
+
+## R Squared Plot
+
+# Drop NaN values from the dataframe
+
+df_onechan = df.loc[df.channel == "O2"]
+
+avgs = df_onechan.groupby(["subject"])["complexity", "discontinuity"].mean()
+
+# Export correlation results
+
+avgs.to_csv('C:/Users/Yasmeen/Desktop/thesis_project/results/final_all/avgs.csv')
+
+# Create the scatterplot
+fig, ax = plt.subplots()
+sns.scatterplot(data=avgs, x="discontinuity", y="complexity", ax=ax, color="purple", alpha = 0.5)
+
+# Add trendline and R-squared value
+sns.regplot(data=avgs, x="discontinuity", y="complexity", scatter=False, ax=ax, ci=None, color = "black")
+slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(avgs["discontinuity"], avgs["complexity"])
+plt.text(0.85, 0.05, fr"$r^2$ = {(r_value**2):.4f}", transform=ax.transAxes, fontsize=12, verticalalignment='top')
+
+# Show the plot
+#plt.show()
+plt.savefig(r'C:\Users\Yasmeen\Desktop\thesis_project\results\final_all\disc_complexity_scatter_ec.png')
+
+
+## Repeated measures
+
+
+# Removing the channels
+
+no_channels = df.groupby(["subject", "session"])["complexity", "discontinuity"].mean().reset_index()
 
 # Export correlation results
 no_channels.to_csv('C:/Users/Yasmeen/Desktop/thesis_project/results/final_all/no_channels.csv', index = False)
 
 no_channels = pd.read_csv('C:/Users/Yasmeen/Desktop/thesis_project/results/final_all/no_channels.csv')
 
-#boxplot of complexity across all channels in all tasks
+# Plot
 
-# Define a dictionary to map old labels to new labels
-label_map = {'EC': 'Resting State', 'Ma': 'Math', 'Me': 'Memory', 'Mu': 'Music'}
+pg.plot_rm_corr(data = no_channels, x ='discontinuity', y = 'complexity', subject = 'subject')
+# Export/save
+plt.savefig(r'C:\Users\Yasmeen\Desktop\thesis_project\results\final_all\plot_rm_corr.png')
+plt.close()
 
-# Replace old labels with new labels in the 'task' column
-df['task'] = df['task'].map(label_map)
+# Stats
 
-sns.boxplot(data = df, x = 'task', y = 'complexity', hue = 'session')
-plt.savefig(r'C:\Users\Yasmeen\Desktop\thesis_project\results\final_all\task_complexity.png')
-plt.show()
-
-#anova of session and task
-
-anova_rm_no_open = pg.rm_anova(data = no_channels, dv = 'complexity', within = ['task','session'], subject ='subject', detailed = True)
-
-anova_rm_no_open.to_csv(r'C:\Users\Yasmeen\Desktop\thesis_project\results\final_all\rm_anova_task_no_open.csv')
+stats_EC = pg.rm_corr(data = no_channels, x ='discontinuity', y = 'complexity', subject = 'subject')
+# Export/save
+stats_EC.to_csv(r'C:\Users\Yasmeen\Desktop\thesis_project\results\final_all\stats_rm_corr.csv')
 
 
-### paiwise tests across the all data  
 
-pairwise = pg.pairwise_tests(data = no_channels, dv = 'complexity', between = 'task', within = 'session', subject = 'subject')
 
-# Add new p-value column that corrects for multiple comparisons
-reject, corrected_pvalues = pg.multicomp(pairwise["p-unc"], method="fdr_bh")
-pairwise["p-val_corrected"] = corrected_pvalues
-
-# Sort dataframe by p-values (lowest at top) for readability
-pairwise = pairwise.sort_values("p-unc")
-
-# Export single dataframe holding all pairwise comparisons
-#stats.to_csv(export_path, index=False)
-
-pairwise.to_csv(r'C:\Users\Yasmeen\Desktop\thesis_project\results\final_all\pairwise.csv', index=False)
